dfc2bfe758dc4e692358f3c9635f694b72daecd9
weru <147871343+marshaweru@users.noreply.github.com>
2023-11-13 23:35:33 -0800

    Function for execution when we type in input box

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 //Create array containing suggested search terms
+let availableKeyWords = [
+    'React JS',
+    'Artificial Intelligence',
+    'LLM',
+    'Python',
+    'Software entrepreneurship',
+    'Food fight',
+    'SaaS',
+    'FullStack engineering',
+    'Back-end engineering',
+    'Mercedes Benz',
+    'Porsche',
+    'F-16',
+    'Audi',
+];
+
+//Add variables for elements
+const resultBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+//Function for execution when we type anything in input box
